c1dc6ee1312cb0f9b95f94d95f2a14bf8b8398f0
slayTheSensei <kylejpearson@gmail.com>
2017-08-28 15:46:53 -0400

    WIP
    Sign in works

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -26,6 +26,7 @@ const failure = (error) => {
 const signInSuccess = (data) => {
   app.user = data.user;
   console.log(app.user);
+  location.href = 'http://localhost:7165/profile.html'
 };
 
 const signOutSuccess = () => {
