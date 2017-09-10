c576db230c971497c3f3254613c80c1d9ef0a1aa
slayTheSensei <kylejpearson@gmail.com>
2017-09-10 19:16:05 -0400

    Deploy

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,8 +1,8 @@
 'use strict'
 
 const app = {
-  // host: 'https://tribe-api.herokuapp.com/'
-  host: 'http://localhost:4741/'
+  host: 'https://telehealth-api.herokuapp.com/'
+  // host: 'http://localhost:4741/'
 }
 
 module.exports = app
